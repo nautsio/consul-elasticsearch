f2e72a10a44f9c8787fc1b5cfc10d61206d145d3
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2015-04-25 15:28:50 +0000

    missed another SERVICE_ID

--- start-es.sh
+++ start-es.sh
@@ -3,7 +3,7 @@
 function getExternalAddress() {
 	curl -s http://consul:8500/v1/catalog/service/$SERVICE_NAME?tag=es-transport  \
 		| /jq -r ".[] | \
-			select(.ServiceID==\"$SERVICE_ID\")  | \
+			select(.ServiceID==\"$SERVICE_9300_ID\")  | \
 			\"export PUBLISH_HOST=\" + .Address,  \
 		\"export PUBLISH_PORT=\" + (.ServicePort | tostring ) " > /publish.env
 }
@@ -11,19 +11,19 @@ function getExternalAddress() {
 function getExternalAddressWait() {
 
 	COUNT=1
-	while [ -z "$PUBLISH_PORT" ] ; do
+	while [ $COUNT -le 60 -a -z "$PUBLISH_PORT" ] ; do
 		getExternalAddress
 		. /publish.env
-		if [ $COUNT -le 60 -a -z "$PUBLISH_PORT" ] ; then
+		if [ -z "$PUBLISH_PORT" ] ; then
 			echo "Failed to obtain publish host and port for service '$SERVICE_9300_ID'. Retrying in 1s.." >&2
 			sleep 1
 			COUNT=$(($COUNT + 1))
 		fi
-		if [ -z "$PUBLISH_PORT" ] ; then
-			echo "Failed to obtain publish host and port for service '$SERVICE_9300_ID'. " >&2
-			exit 1
-		fi
 	done
+	if [ -z "$PUBLISH_PORT" ] ; then
+		echo "Failed to obtain publish host and port for service '$SERVICE_9300_ID'. " >&2
+		exit 1
+	fi
 }
 sed -i -e 's/es-transport.[^"]*/es-transport.'"$SERVICE_NAME/" /es-unicast.ctmpl
 /consul-template -consul consul:8500 -once -template /es-unicast.ctmpl:/es-unicast.lst 
