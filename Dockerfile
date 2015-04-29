6028e0d9e99652498c7f8901be6177e1b10c7e1f
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2015-04-29 21:24:35 +0200

    cleaned up start script

--- Dockerfile
+++ Dockerfile
@@ -5,43 +5,12 @@
 #
 
 # Pull base image.
-FROM dockerfile/java:oracle-java8
+FROM elasticsearch:1.5.1
 
-ENV ES_PKG_NAME elasticsearch-1.5.0
+ADD http://stedolan.github.io/jq/download/linux64/jq /usr/local/bin/jq
+RUN chmod +x /usr/local/bin/jq
 
-ADD https://github.com/hashicorp/consul-template/releases/download/v0.8.0/consul-template_0.8.0_linux_amd64.tar.gz  /consul-template.tar.gz
-RUN cd / && tar xzvf /consul-template.tar.gz --strip-components=1 && rm /consul-template.tar.gz
+ADD /start-elasticsearch-clustered.sh /
+RUN chmod +x /start-elasticsearch-clustered.sh
 
-ADD http://stedolan.github.io/jq/download/linux64/jq /jq
-RUN chmod +x /jq
-
-# Install Elasticsearch.
-RUN \
-  cd / && \
-  curl -s https://download.elasticsearch.org/elasticsearch/elasticsearch/$ES_PKG_NAME.tar.gz | \
-  tar xvzf - && \
-  mv /$ES_PKG_NAME /elasticsearch
-
-RUN cd / && \
-     /elasticsearch/bin/plugin -install mobz/elasticsearch-head
-
-# Define mountable directories.
-VOLUME ["/data"]
-
-# Mount elasticsearch.yml config
-ADD config/elasticsearch.yml /elasticsearch/config/elasticsearch.yml
-ADD /es-unicast.ctmpl /
-ADD /start-es.sh /
-RUN chmod +x /start-es.sh
-
-# Define working directory.
-WORKDIR /data
-
-# Define default command.
-CMD ["/start-es.sh"]
-
-# Expose ports.
-#   - 9200: HTTP
-#   - 9300: transport
-EXPOSE 9200
-EXPOSE 9300
+CMD ["/start-elasticsearch-clustered.sh"]
---
+++ start-elasticsearch-clustered.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+PEERS=${TMP:-/tmp}/$SERVICE_NAME.json
+
+function getServiceAddressesFromConsul() {
+        curl -s http://consul:8500/v1/catalog/service/$SERVICE_NAME?tag=es-transport > $PEERS
+}
+
+function getOwnPublishPort() {
+        jq -r ".[] | select(.ServiceID==\"$SERVICE_9300_ID\") | .ServicePort" $PEERS
+}
+
+function getOwnPublishAddress() {
+        jq -r ".[] | select(.ServiceID==\"$SERVICE_9300_ID\") | .Address" $PEERS
+}
+
+function getNumberOfPeers() {
+        jq -r 'length'  $PEERS
+}
+
+function getPeerAddressList() {
+        jq -r '[ .[] | [ .Address, .ServicePort | tostring ] | join(":")  ] | join(",")'  $PEERS
+}
+
+function waitForAllServers() {
+        COUNT=0
+        NR_OF_SERVERS=0
+        while [ $COUNT -lt 60 -a $NR_OF_SERVERS -lt $TOTAL_NR_OF_SERVERS ] ; do
+                getServiceAddressesFromConsul
+                NR_OF_SERVERS=$(getNumberOfPeers)
+                if [ $NR_OF_SERVERS -lt $TOTAL_NR_OF_SERVERS ] ; then
+                        echo "$NR_OF_SERVERS found in registry, $TOTAL_NR_OF_SERVERS required. sleeping 2s." >&2
+                        sleep 2
+                        COUNT=$(($COUNT + 1))
+                fi
+        done
+        if [ $NR_OF_SERVERS -lt $TOTAL_NR_OF_SERVERS ] ; then
+                echo "Failed to acquire the required number of services" >&2
+                exit 1
+        fi
+}
+
+if [ -z "$TOTAL_NR_OF_SERVERS" ] ; then
+	echo "ERROR: Environment variable TOTAL_NR_OF_SERVERS is not specified." >&2
+	exit 1
+fi
+
+if [ -z "$SERVICE_9300_ID" ] ; then
+	echo "ERROR: Environment variable SERVICE_9300_ID is not set. requires to find my own publish port in Consul." >&2
+	exit 1
+fi
+
+if [ -z "$SERVICE_NAME" ] ; then
+	echo "ERROR: Environment variable SERVICE_NAME is not set. requires to find peers own publish port and peers in cluster." >&2
+	exit 1
+fi
+
+if [ $? -eq 0 ] ; then
+        waitForAllServers
+        PUBLISH_PORT=$(getOwnPublishPort)
+        PUBLISH_ADDRESS=$(getOwnPublishAddress)
+        HOST_LIST=$(getPeerAddressList)
+        echo INFO: my network endpoint: $PUBLISH_ADDRESS:$PUBLISH_PORT
+        echo INFO: other servers: $HOST_LIST
+
+	if [ -z "$PUBLISH_PORT" -o -z "$PUBLISH_ADDRESS" -o -z "HOST_LIST" ] ; then
+		echo ERROR: Failed to satisfy pre-conditions to start this node.
+	fi
+
+        exec gosu elasticsearch \
+                --discovery.zen.ping.multicast.enabled=false \
+                --discovery.zen.ping.unicast.hosts=$HOST_LIST \
+                --transport.publish_host=$PUBLISH_HOST \
+                --transport.publish_port=$PUBLISH_PORT \
+                --cluster.name=$SERVICE_NAME \
+                --node.name=$SERVICE_9300_ID \
+                $@
+else
+	echo ERROR: consul-template exited with non-zero status: $? >&2
+	exit $?
+fi
